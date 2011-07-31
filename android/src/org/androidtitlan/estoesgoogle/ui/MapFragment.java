e2d3560e73d35d956608843611391d3135372c8c
Enrique Diaz <nrikediaz@androidtitlan.org>
2011-07-31 09:05:48 -0500

    Adding map section using WebView

--- android/src/org/androidtitlan/estoesgoogle/ui/MapFragment.java
+++ android/src/org/androidtitlan/estoesgoogle/ui/MapFragment.java
@@ -52,49 +52,57 @@ public class MapFragment extends Fragment {
     public static final String EXTRA_ROOM = "com.google.android.iosched.extra.ROOM";
 
     private static final String MAP_JSI_NAME = "MAP_CONTAINER";
-    private static final String MAP_URL = "http://www.google.com/events/io/2011/embed.html";
+    private static final String MAP_URL = "http://maps.google.com/maps?q=Hip%C3%B3dromo+de" +
+    		"+Las+Americas,+Lomas+de+Sotelo,+Miguel+Hidalgo,+Ciudad+de+M%C3%A9xico," +
+    		"+M%C3%A9xico&hl=es&ie=UTF8&ll=19.438246,-99.2224&spn=" +
+    		"0.008944,0.016115&sll=37.0625,-95.677068&sspn=30.819956," +
+    		"66.005859&geocode=FYmbKAEdUvwV-g&z=16";
     private static boolean CLEAR_CACHE_ON_LOAD = false;
 
     private WebView mWebView;
     private View mLoadingSpinner;
     private boolean mMapInitialized = false;
 
+	private WebChromeClient mWebChromeClient;
+
+	private WebViewClient mWebViewClient;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         AnalyticsUtils.getInstance(getActivity()).trackPageView("/Map");
-        }
+        } 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.mapview, null);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_webview_with_spinner, null);
 
         // For some reason, if we omit this, NoSaveStateFrameLayout thinks we are
         // FILL_PARENT / WRAP_CONTENT, making the progress bar stick to the top of the activity.
         root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
                 ViewGroup.LayoutParams.FILL_PARENT));
 
-//        mLoadingSpinner = root.findViewById(R.id.loading_spinner);
-//        mWebView = (WebView) root.findViewById(R.id.webview);
-//        mWebView.setWebChromeClient(mWebChromeClient);
-//        mWebView.setWebViewClient(mWebViewClient);
-//
-//        mWebView.post(new Runnable() {
-//            public void run() {
-//                // Initialize web view
-//                if (CLEAR_CACHE_ON_LOAD) {
-//                    mWebView.clearCache(true);
-//                }
-//
-//                mWebView.getSettings().setJavaScriptEnabled(true);
-//                mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
-//                mWebView.loadUrl(MAP_URL);
-////                mWebView.addJavascriptInterface(mMapJsiImpl, MAP_JSI_NAME);
-//            }
-//        });
+        mLoadingSpinner = root.findViewById(R.id.loading_spinner);
+        mWebView = (WebView) root.findViewById(R.id.webview);
+        mWebView.setWebChromeClient(mWebChromeClient);
+        mWebView.setWebViewClient(mWebViewClient);
+
+        mWebView.post(new Runnable() {
+            public void run() {
+                // Initialize web view
+                if (CLEAR_CACHE_ON_LOAD) {
+                    mWebView.clearCache(true);
+                }
+
+                mWebView.getSettings().setJavaScriptEnabled(true);
+                mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
+                mWebView.loadUrl(MAP_URL);
+//                mWebView.addJavascriptInterface(mMapJsiImpl, MAP_JSI_NAME);
+            }
+        });
 
         return root;
     }
